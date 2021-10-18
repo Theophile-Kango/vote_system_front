384b4bb4133bbc5fc0934cd62d79eedfb90fd91f
Theophile Kango <fadhili.kango@gmail.com>
2021-10-18 20:48:07 +0200

    Petit titre dans accueil

--- App.js
+++ App.js
@@ -46,7 +46,7 @@ const App = () => {
               headerTintColor: "#fff",
               headerTitleStyle: {
                 fontWeight: 'bold',
-                fontSize: 15
+                fontSize: 10
               }
             })
           }
